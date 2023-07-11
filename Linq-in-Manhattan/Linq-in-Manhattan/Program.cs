3fe89be812408e9d74962474a8ab580d3d7b56c7
alaaabusalem <alaa.abusalem94@hotmail.com>
2023-07-11 08:40:58 +0300

    new branch

---
+++ Linq-in-Manhattan/Linq-in-Manhattan/Program.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace Linq_in_Manhattan
+{
+
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			string JsonFilePath = "../../../data.json";
+			string JsonDataString = File.ReadAllText(JsonFilePath);
+		    
+			Root root= new Root();
+			var JsonObj=JsonConvert.DeserializeObject<Root>(JsonDataString);
+			// find neighborhoods
+			var neighborhoods = JsonObj.features.Where(item=> item.properties.neighborhood != null);
+			int countNeighborhoods = 0;
+			  foreach(var neighborhood in neighborhoods) {
+				countNeighborhoods++;	
+
+			}
+			  Console.WriteLine($"1.we have {countNeighborhoods} neighborhoods");
+
+			// find  neighborhoods that do not have any names
+			var neighborhoodsNoName = JsonObj.features.Where(item => item.properties.neighborhood != null && item.properties.neighborhood !="");
+			int countneighborhoodsNoName = 0;
+			foreach (var neighborhood in neighborhoodsNoName)
+
+			{
+				countneighborhoodsNoName++;
+
+			}
+			Console.WriteLine($"2.we have {countneighborhoodsNoName} neighborhoods with Names");
+
+			// Remove the duplicates
+
+			var neighborhoodsWithoutDuplicate = JsonObj.features.Where(item => item.properties.neighborhood != "").Select(item => item.properties.neighborhood).Distinct();
+			int countneighborhoodsWithoutDuplicate = 0;
+			foreach (var neighborhood in neighborhoodsWithoutDuplicate)
+
+			{
+				countneighborhoodsWithoutDuplicate++;
+
+			}
+			Console.WriteLine($"3.we have {countneighborhoodsWithoutDuplicate} neighborhoods without duplicates");
+
+			//Rewrite the queries from above and consolidate all into one single query.
+
+			// Rewrite at least one of these questions only using the opposing method (example: Use LINQ Query statements instead of LINQ method calls and vice versa.)
+
+			Console.WriteLine();
+			Console.WriteLine("***Output all of the neighborhoods in this data list using LINQ Query statements");
+			var ListOfneighborhoods=from feature
+									in JsonObj.features
+									where feature.properties.neighborhood != null
+									select feature;
+
+			int countListOfneighborhoods = 0;
+			foreach (var neighborhood in neighborhoods)
+			{
+				countListOfneighborhoods++;
+
+			}
+
+			Console.WriteLine($"5.we have {countListOfneighborhoods} neighborhoods");
+		}
+	}
+	
+	
+
+	
+
+	
+
+	
+}
---
+++ Linq-in-Manhattan/Linq-in-Manhattan/README.md
@@ -0,0 +1,14 @@
+# Manhattan Neighborhoods 
+My App contain a json file with data about Manhattan Neighborhoods thier names and coordinates etc.
+I used the Linq and lamda expression
+to find some information and show it in the console
+ as shown in the picture
+
+![console App](Lab9.png)
+
+I created a Feacture,Geometry,Root classes 
+and addedd the data.json file to the project
+and used the I/O to read the file, then use  LINQ method
+statment to return some data as requierd and I used one time Query statement
+as required.
+
